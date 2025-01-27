3f2a7f2888fd32d44f6d72edc9f7c6c07cd539ac
Robin Singh <robin.singh@ymipl.in>
2025-01-27 11:48:25 +0530

    nothing to say

--- app/comments/[id]/route.ts
+++ app/comments/[id]/route.ts
@@ -1,46 +1,36 @@
-// import { comments } from "../data"
-// /**
-//  * Handles the GET request and returns a response with a new comment.
-//  * 
-//  * @returns {Promise<Response>} A promise that resolves to a Response object containing the message "New Comment".
-//  */
-// // import { NextResponse } from "next/server";
+import { comments } from "../data"
+/**
+ * Handles the GET request and returns a response with a new comment.
+ * 
+ * @returns {Promise<Response>} A promise that resolves to a Response object containing the message "New Comment".
+ */
+export async function GET(_req: Request, { params }: { params: { id: string } }) {
+    const comment = comments.find(
+        (comment) => comment.id === parseInt(params.id)
+    );
+    return Response.json(comment);
+}
 
-// export async function GET(
-//     _req: Request,
-//     { params }: { params: { id: string } }
-// ) {
-//     const comment = comments.find(
-//         (comment) => comment.id === parseInt(params.id, 10) // Ensure parsing with base 10
-//     );
+export async function PATCH(req: Request, { params }: { params: { id: string } }) {
+    const body = await req.json();
 
-//     if (!comment) {
-//         return Response.json({ error: "Comment not found" }, { status: 404 });
-//     }
+    const { text } = body
 
-//     return Response.json(comment);
-// }
+    const index = comments.findIndex(
+        comment => comment.id === parseInt(params.id)
+    )
 
-// export async function PATCH(req: Request, { params }: { params: { id: string } }) {
-//     const body = await req.json();
+    comments[index].text = text;
 
-//     const { text } = body
+    return Response.json(comments[index]);
+}
 
-//     const index = comments.findIndex(
-//         comment => comment.id === parseInt(params.id)
-//     )
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+    const index = comments.findIndex(comment => comment.id === parseInt(params.id));
 
-//     comments[index].text = text;
+    const deletedComment = comments[index];
+    comments.splice(index, 1);
 
-//     return Response.json(comments[index]);
-// }
+    return Response.json(deletedComment);
 
-// export async function DELETE(req: Request, { params }: { params: { id: string } }) {
-//     const index = comments.findIndex(comment => comment.id === parseInt(params.id));
-
-//     const deletedComment = comments[index];
-//     comments.splice(index, 1);
-
-//     return Response.json(deletedComment);
-
-// }
+}
